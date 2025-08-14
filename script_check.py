02e80960d9b9ea226c653184b226b46817a2f7b6
nathan3-14 <nathan.watson.172@gmail.com>
2025-08-14 23:13:23 +0100

    Improved script check

--- script_check.py
+++ script_check.py
@@ -7,7 +7,10 @@ if __name__ == "__main__":
         print("Invalid args")
         quit()
     
-    path = f"scripts/{args[0]}/{args[0]}.json"
+    if ".json" in args[0]:
+        path = args[0]
+    else:
+        path = f"scripts/{args[0]}/{args[0]}.json"
     try:
         script = json.load(open(path, "r"))
     except FileNotFoundError:
@@ -15,13 +18,14 @@ if __name__ == "__main__":
         quit()
     
     character_check_list = json.load(open("characters.json", "r"))
+    travellers_list = json.load(open("travellers.json", "r"))
     
     valid = True
     for character in script:
         id = character["id"]
         if id == "_meta":
             continue
-        if id not in character_check_list:
+        if id not in character_check_list and id not in travellers_list:
             print(f"Script is invalid, character '{id}' cannot be used")
             valid = False
     
