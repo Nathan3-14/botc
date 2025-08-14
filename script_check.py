877100be874e5c89332b7410215ae3d97a8d8566
nathan3-14 <nathan.watson.172@gmail.com>
2025-08-14 22:27:01 +0100

    Added script checking

---
+++ script_check.py
@@ -0,0 +1,29 @@
+import json
+from sys import argv
+
+if __name__ == "__main__":
+    args = argv[1:]
+    if len(args) != 1:
+        print("Invalid args")
+        quit()
+    
+    path = f"scripts/{args[0]}/{args[0]}.json"
+    try:
+        script = json.load(open(path, "r"))
+    except FileNotFoundError:
+        print(f"File './{path}' does not exist")
+        quit()
+    
+    character_check_list = json.load(open("characters.json", "r"))
+    
+    valid = True
+    for character in script:
+        id = character["id"]
+        if id == "_meta":
+            continue
+        if id not in character_check_list:
+            print(f"Script is invalid, character '{id}' cannot be used")
+            valid = False
+    
+    if valid:
+        print(f"Script is usable!")
