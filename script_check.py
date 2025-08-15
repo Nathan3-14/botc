c19c0a98009ba30685038097deaf0ffa2bc33b25
nathan3-14 <nathan.watson.172@gmail.com>
2025-08-15 15:43:12 +0100

    Improved script check

--- script_check.py
+++ script_check.py
@@ -1,20 +1,22 @@
 import json
 from sys import argv
+import os
 
 if __name__ == "__main__":
+    current_path = os.getcwd()
+    scripts_path = os.path.join(current_path, "scripts")
+
     args = argv[1:]
     if len(args) != 1:
         print("Invalid args")
         quit()
     
-    if ".json" in args[0]:
-        path = args[0]
-    else:
-        path = f"scripts/{args[0]}/{args[0]}.json"
+    current_script_directory = os.path.join(scripts_path, args[0])
+    current_script_path = os.path.join(current_script_directory, args[0] + "" if args[0].endswith(".json") else ".json")
     try:
-        script = json.load(open(path, "r"))
+        script = json.load(open(current_script_path, "r"))
     except FileNotFoundError:
-        print(f"File './{path}' does not exist")
+        print(f"File '{current_script_path}' does not exist")
         quit()
     
     character_check_list = json.load(open("characters.json", "r"))
