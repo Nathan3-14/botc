f485568858559c042f791e1247fd875fa75d9698
nathan3-14 <nathan.watson.172@gmail.com>
2025-08-15 18:53:25 +0100

    Added base 3 scripts

--- script_tools.py
+++ script_tools.py
@@ -15,24 +15,19 @@ if __name__ == "__main__":
                 print(f"File '{args[1]}' doesn't exist")
                 quit()
             current_script_file = args[1]
-            current_script_name = ".".join(current_script_file.split(".")[:-1])
+            current_script_name = ".".join(current_script_file.split(".")[:-1]).strip("./\\")
+            print(current_script_name)
             current_script_path = join_path(SCRIPTS_PATH, current_script_name)
             if not os.path.exists(current_script_path):
                 os.mkdir(current_script_path)
-            os.rename(join_path(CURRENT_PATH, args[1]), join_path(current_script_path, args[1]))
+            os.rename(join_path(CURRENT_PATH, args[1]), join_path(current_script_path, f"{current_script_name}.json"))
             if not check(current_script_name):
                 quit()
             format(current_script_name)
         case "check":
-            if not os.path.exists(args[1]):
-                print(f"File '{args[1]}' doesn't exist")
-                quit()
-            check(".".join(args[1].split(".")[:-1]))
+            check(args[1])
         case "format":
-            if not os.path.exists(args[1]):
-                print(f"File '{args[1]}' doesn't exist")
-                quit()
-            format(".".join(args[1].split(".")[:-1]))
+            format(args[1])
         case _:
             print(f"Invalid option '{args[0]}', expected 'new', 'check' or 'format'")
             quit()
